fcad8a5ef068c02d5c574dd0bd5c0ac9e82297d5
Matthew Barker <mbarker@softwareinstitute.com>
2022-02-04 14:49:12 +0000

    8, more tests

--- src/test/java/com/TSI/ITSgame/Test/CharacterTest.java
+++ src/test/java/com/TSI/ITSgame/Test/CharacterTest.java
@@ -72,6 +72,58 @@ public class CharacterTest {
 
     }
 
+    @Test
+    public void testMoveLeftMiddleBoard() {
+
+        Board dumby = new Board();
+        dumby.initialise();
+        Character testPlayer = new Character(6,6, "Big Jeff");
+
+        testPlayer.moveLeft(dumby);
+
+        assertEquals(5, testPlayer.getPlayerPosX(), "That's not the correct new coordinate.");
+
+    }
+
+    @Test
+    public void testMoveLeftSideBoard() {
+
+        Board dumby = new Board();
+        dumby.initialise();
+        Character testPlayer = new Character(0,6, "Big Jeff");
+
+        testPlayer.moveLeft(dumby);
+
+        assertEquals(11, testPlayer.getPlayerPosX(), "That's not the correct new coordinate.");
+
+    }
+
+    @Test
+    public void testMoveRightSideBoard() {
+
+        Board dumby = new Board();
+        dumby.initialise();
+        Character testPlayer = new Character(11,6, "Big Jeff");
+
+        testPlayer.moveRight(dumby);
+
+        assertEquals(0, testPlayer.getPlayerPosX(), "That's not the correct new coordinate.");
+
+    }
+
+    @Test
+    public void testMoveRightMiddleBoard() {
+
+        Board dumby = new Board();
+        dumby.initialise();
+        Character testPlayer = new Character(6,6, "Big Jeff");
+
+        testPlayer.moveRight(dumby);
+
+        assertEquals(7, testPlayer.getPlayerPosX(), "That's not the correct new coordinate.");
+
+    }
+
 
 
 }
